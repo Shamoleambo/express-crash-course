c066837c0c9312827f2998c384a2a941cb0094a8
Tiago Gomes <tidgomes@gmail.com>
2021-12-08 11:00:47 -0300

    nodemon added as a dev-dependency and route for simple get / request created

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("<h1>Hello Express</h1>");
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is runnig on port ${PORT}`));
