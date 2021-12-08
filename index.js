60ff1d007dfdc1f55af4ae63ec7f789a7a34f2a6
Tiago Gomes <tidgomes@gmail.com>
2021-12-08 14:24:49 -0300

    request with paramethers passed on the url, and a if control structure for bad requests

--- index.js
+++ index.js
@@ -2,19 +2,25 @@ const express = require("express");
 const path = require("path");
 
 const members = require("./Members.js");
+const logger = require("./middleware/logger");
 
 const app = express();
 
-const logger = (req, res, next) => {
-  console.log(`${req.protocol}//${req.get("host")}${req.originalUrl}`);
-
-  next();
-};
-
-app.use(logger);
+// app.use(logger);
 
 app.get("/api/members", (req, res) => {
   res.json(members);
+});
+
+app.get("/api/members/:id", (req, res) => {
+  const urlParamsId = parseInt(req.params.id);
+  const found = members.some((member) => member.id === urlParamsId);
+
+  if (found) {
+    res.json(members.filter((member) => member.id === urlParamsId));
+  } else {
+    res.status(400).json({ msg: `Member ${urlParamsId} not found` });
+  }
 });
 
 app.use(express.static(path.join(__dirname, "public")));
